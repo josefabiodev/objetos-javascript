584b6ad76f34df8793b5a80470e5cc209c04c448
josefabiodev <josefabiodev@gmail.com>
2023-10-27 09:25:01 -0300

    Funções com objetos

---
+++ funcoes.js
@@ -0,0 +1,21 @@
+const cliente = {
+    nome: "João",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: {
+        residencial: "99 9999-9999",
+        celular: "99 99999-9999",
+    },
+    saldo: 200,
+    efetuaPagamento: function (valor) {
+        if(valor > this.saldo) {
+            console.log("Saldo insuficiente.")
+        }else {
+            this.saldo -= valor;
+            console.log(`Pagamento efetuado com sucesso. Novo saldo: ${this.saldo}`)
+        }
+    },
+};
+
+cliente.efetuaPagamento(250);
+cliente.efetuaPagamento(25);
